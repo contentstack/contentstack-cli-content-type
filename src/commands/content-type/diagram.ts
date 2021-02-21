c5eee317bcc28e43333cd4e3c0b91c884fc6894a
Michael Davis <micdavis@live.com>
2021-02-21 09:38:39 -0600

    creating output options

--- src/commands/content-type/diagram.ts
+++ src/commands/content-type/diagram.ts
@@ -1,14 +1,16 @@
 import Command from '../../core/command'
 import {flags} from '@contentstack/cli-command'
 import cli from 'cli-ux'
-import buildOutput from '../../core/content-type/list'
+import {createDiagram, CreateDiagramOptions} from '../../core/content-type/diagram'
 
 export default class DiagramCommand extends Command {
   static description = 'create a visual diagram of a Stack\'s Content Types';
 
   static examples = [
-    '$ csdx content-type:diagram -s "xxxxxxxxxxxxxxxxxxx"',
-    '$ csdx content-type:diagram -a "management token"',
+    '$ csdx content-type:diagram -s "xxxxxxxxxxxxxxxxxxx" -o "./content-model.svg"',
+    '$ csdx content-type:diagram -a "management token" -o "./content-model.svg"',
+    '$ csdx content-type:diagram -a "management token" -o "./content-model.svg" -d "landscape"',
+    '$ csdx content-type:diagram -a "management token" -o "./content-model.dot" -t "dot"',
   ];
 
   static flags = {
@@ -24,7 +26,35 @@ export default class DiagramCommand extends Command {
       char: 'a',
       description: 'management token alias',
       required: false,
-    })
+    }),
+  
+    output: flags.string({
+      char: 'o',
+      description: 'full path to output',
+      hidden: false,
+      multiple: false,
+      required: true,
+    }),
+
+    direction: flags.string({
+      char: 'd',
+      description: 'graph orientation',
+      default: 'portrait',
+      options: ['portrait', 'landscape'],
+      hidden: false,
+      multiple: false,
+      required: true,
+    }),
+
+    type: flags.string({
+      char: 't',
+      description: 'graph output file type',
+      default: 'svg',
+      options: ['svg', 'dot'],
+      hidden: false,
+      multiple: false,
+      required: true,
+    }),
   }
 
   async run() {
@@ -32,17 +62,36 @@ export default class DiagramCommand extends Command {
       const {flags} = this.parse(DiagramCommand)
       this.setup(flags)
 
+      const outputPath = flags.output
+
+      if (!outputPath || !outputPath.trim()) {
+        this.error('Please provide an output path.', {exit: 2})
+      }
+
       cli.action.start(Command.RequestDataMessage)
 
       const [stack, contentTypes, globalFields] = await Promise.all([
         this.client.getStack(this.apiKey),
         this.client.getContentTypes(this.apiKey, false),
-        this.client.getGlobalFields(this.apiKey)
+        this.client.getGlobalFields(this.apiKey),
       ])
 
       cli.action.stop()
 
-      console.log(globalFields);
+
+      const diagramOptions: CreateDiagramOptions = {
+        stackName: stack.name,
+        contentTypes: contentTypes.content_types,
+        globalFields: globalFields.global_fields,
+        outputFileName: outputPath,
+        outputFileType: flags.type,
+        style: {
+          orientation: flags.direction === 'portrait' ? 'LR' : 'TD'
+        }
+      }
+
+      const output = await createDiagram(diagramOptions)
+      this.log(`Created Graph: ${output.outputPath}`)
 
     } catch (error) {
       this.error(error, {exit: 1, suggestions: error.suggestions})
--- src/core/content-type/diagram.ts
+++ src/core/content-type/diagram.ts
@@ -1,6 +1,7 @@
+import * as path from 'path'
 import * as fs from 'fs'
 import moment from 'moment'
-const { graphviz } = require('node-graphviz')
+const {graphviz} = require('node-graphviz')
 
 enum DiagramNodeType {
     ContentType,
@@ -10,7 +11,7 @@ enum DiagramNodeType {
 interface DiagramNode {
     uid: string;
     title: string;
-    fields: any[]
+    fields: any[];
 }
 
 interface DiagramNodeField {
@@ -25,132 +26,156 @@ interface DiagramNodeField {
     unique: boolean;
 }
 
-export async function createDiagram(stackName: string, contentTypes: any[], globalFields: any[], outputFileName: string, outputFileType: string): Promise<any> {
-    const contentTypeNodes = createNodes(contentTypes);
-    const globalFieldNodes = createNodes(globalFields);
-    const now = moment().format("MMMM Do YYYY, h:mm:ss a");
-    const graphLabel = `${stackName} | ${now}`
+export interface CreateDiagramOptions {
+  stackName: string;
+  contentTypes: any[];
+  globalFields: any[];
+  outputFileName: string;
+  outputFileType: string;
+  style: DiagramStyleOptions;
+}
 
-    const dotSource = createDotSource(graphLabel, contentTypeNodes, globalFieldNodes);    
+export interface CreateDiagramResults {
+  outputPath: string;
+}
 
-    const dotOutput = await graphviz.dot(dotSource, outputFileType);
-    fs.writeFileSync(outputFileName, dotOutput);
+export interface DiagramStyleOptions {
+  orientation: 'LR' | 'TD';
+}
+
+export async function createDiagram(options: CreateDiagramOptions): Promise<CreateDiagramResults> {
+  const contentTypeNodes = createNodes(options.contentTypes)
+  const globalFieldNodes = createNodes(options.globalFields)
+  const now = moment().format('MMMM Do YYYY, h:mm:ss a')
+  const graphLabel = `${options.stackName} | ${now}\n\n`
+
+  const dotSource = createDotSource(graphLabel, contentTypeNodes, globalFieldNodes, options.style)
+
+  const dotOutput = await graphviz.dot(dotSource, options.outputFileType)
+  const outputPath = createOutputPath(options.outputFileName)
+
+  fs.writeFileSync(outputPath, dotOutput)
+
+  return {
+    outputPath
+  }
 }
 
 const theme = {
-    graph: {
-        fontname: 'Helvetica',
-        fontsize: '120',
-        fontcolor: '#2a0f57'
-    },
-    
-    node: {
-        fontname: 'Helvetica',
-        fontsize: '12',
-        fontcolor: '#555555'
-    },
+  graph: {
+    fontname: 'Helvetica',
+    fontsize: '120',
+    fontcolor: '#2a0f57',
+  },
 
-    edge: {
-        fontname: 'Helvetica',
-        fontsize: '11',
-        color: '#2a0f57'
-    },
+  node: {
+    fontname: 'Helvetica',
+    fontsize: '12',
+    fontcolor: '#555555',
+  },
 
-    table: {
-        heading: {
-            bgcolor: '#7c4dff',
-            color: '#bab7c7',
-            label: {
-                color: 'white'
-            }
-        },
-        row: {
-            bgcolor: '#f7f7f7'
-        }
-    },
+  edge: {
+    fontname: 'Helvetica',
+    fontsize: '11',
+    color: '#2a0f57',
+  },
 
-    entities: {
-        hasOption: '&#x2713;',
-        spacer: '&mdash;'
-    }
+  table: {
+    heading: {
+      bgcolor: '#7c4dff',
+      color: '#bab7c7',
+      label: {
+        color: 'white',
+      },
+    },
+    row: {
+      bgcolor: '#f7f7f7',
+    },
+  },
+
+  entities: {
+    hasOption: '&#x2713;',
+    spacer: '&mdash;',
+  },
 }
 
 function createNodes(contentTypes: any[]) {
-    const result = [] as DiagramNode[];
+  const result = [] as DiagramNode[]
 
-    for (const contentType of contentTypes) {
-        result.push({
-            uid: contentType.uid,
-            title: contentType.title,
-            fields: createNodeFields(contentType)
-        });
-    }
+  for (const contentType of contentTypes) {
+    result.push({
+      uid: contentType.uid,
+      title: contentType.title,
+      fields: createNodeFields(contentType),
+    })
+  }
 
-    return result;
+  return result
 }
 
 function createNodeFields(contentType: any) {
-    const result = [] as DiagramNodeField[];
+  const result = [] as DiagramNodeField[]
 
-    for (const field of contentType.schema) {
-        const diagramField = {
-            uid: field.uid,
-            title: field.display_name,
-            type: field.data_type,
-            path: null,
-            fields: [],
-            references: [],
-            multiple: field.multiple,
-            mandatory: field.mandatory,
-            unique: field.unique
-        } as DiagramNodeField;
+  for (const field of contentType.schema) {
+    const diagramField = {
+      uid: field.uid,
+      title: field.display_name,
+      type: field.data_type,
+      path: null,
+      fields: [],
+      references: [],
+      multiple: field.multiple,
+      mandatory: field.mandatory,
+      unique: field.unique,
+    } as DiagramNodeField
 
-        if (field.data_type === 'reference') {
-            diagramField.references = field.reference_to;
-        } else if (field.data_type === 'global_field') {
-            diagramField.references = [field.reference_to];
-        }
-
-        if (field.schema) {
-            diagramField.fields = createNodeFields(field);
-        } else if (field.blocks) {
-            for (const block of field.blocks) {
-                const diagramBlockField = {
-                    uid: block.uid,
-                    title: block.title,
-                    type: null,
-                    path: null,
-                    fields: [],
-                    references: [],
-                    multiple: block.multiple,
-                    mandatory: block.mandatory,
-                    unique: block.unique
-                } as DiagramNodeField;
-
-                if (block.schema) {
-                    diagramBlockField.fields = createNodeFields(block);
-                } else {
-                    diagramBlockField.type = 'global_field';
-                    diagramBlockField.references = [block.reference_to];
-                }
-
-                diagramField.fields.push(diagramBlockField);
-            }
-        }
-
-        result.push(diagramField);
+    if (field.data_type === 'reference') {
+      diagramField.references = field.reference_to
+    } else if (field.data_type === 'global_field') {
+      diagramField.references = [field.reference_to]
     }
 
-    return result;
+    if (field.schema) {
+      diagramField.fields = createNodeFields(field)
+    } else if (field.blocks) {
+      for (const block of field.blocks) {
+        const diagramBlockField = {
+          uid: block.uid,
+          title: block.title,
+          type: null,
+          path: null,
+          fields: [],
+          references: [],
+          multiple: block.multiple,
+          mandatory: block.mandatory,
+          unique: block.unique,
+        } as DiagramNodeField
+
+        if (block.schema) {
+          diagramBlockField.fields = createNodeFields(block)
+        } else {
+          diagramBlockField.type = 'global_field'
+          diagramBlockField.references = [block.reference_to]
+        }
+
+        diagramField.fields.push(diagramBlockField)
+      }
+    }
+
+    result.push(diagramField)
+  }
+
+  return result
 }
 
-function createDotSource(graphLabel: string, contentTypeNodes: DiagramNode[], globalFieldNodes: DiagramNode[]) {
-    const result = `
+function createDotSource(graphLabel: string, contentTypeNodes: DiagramNode[], globalFieldNodes: DiagramNode[], style: DiagramStyleOptions) {
+  const result = `
         digraph content_model {
             label="${graphLabel}"
             labelloc="t"
             splines="true"
             clusterrank="global"
+            rankdir="${style.orientation}"
 
             graph [
                 fontname="${theme.graph.fontname}", 
@@ -176,118 +201,130 @@ function createDotSource(graphLabel: string, contentTypeNodes: DiagramNode[], gl
                 penwidth="1"
             ]
 
-            ${ createDotNodes(globalFieldNodes, DiagramNodeType.GlobalFields) }
-            ${ createDotNodes(contentTypeNodes, DiagramNodeType.ContentType) }
+            ${createDotNodes(globalFieldNodes, DiagramNodeType.GlobalFields)}
+            ${createDotNodes(contentTypeNodes, DiagramNodeType.ContentType)}
             
-            ${ createDotEdges(globalFieldNodes) }
-            ${ createDotEdges(contentTypeNodes) }
+            ${createDotEdges(globalFieldNodes)}
+            ${createDotEdges(contentTypeNodes)}
         }
-    `;
+    `
 
-    return result;
+  return result
 }
 
 function createDotNodes(nodes: DiagramNode[], type: DiagramNodeType) {
-    return nodes.map((node, _) => {
-        return createDotTable(node, type);
-    }).join("\n");
+  return nodes.map((node, _) => {
+    return createDotTable(node, type)
+  }).join('\n')
 }
 
 function createDotTable(node: DiagramNode, type: DiagramNodeType) {
-    const nodeLabel = type == DiagramNodeType.GlobalFields ? 'Global Field' : 'Content Type';
+  const nodeLabel = type === DiagramNodeType.GlobalFields ? 'Global Field' : 'Content Type'
 
-    return `
+  return `
         ${node.uid} [label=<
             <table color="${theme.table.heading.color}" border="0" cellborder="1" cellspacing="1" cellpadding="4">
                 <tr>
-                    <td border="0" cellspacing="0" align="center" color="${theme.table.heading.color}" bgcolor="${theme.table.heading.bgcolor}">
-                        <font color="${theme.table.heading.label.color}">${nodeLabel}</font>
-                    </td>
-                    <td border="0" cellspacing="0" colspan="5" color="${theme.table.heading.color}" bgcolor="${theme.table.heading.bgcolor}">
-                        <font color="${theme.table.heading.label.color}">${node.title}</font>
+                    <td cellspacing="0" colspan="6" align="center" color="${theme.table.heading.color}" bgcolor="${theme.table.heading.bgcolor}">
+                        <font color="${theme.table.heading.label.color}"><b>${node.title}</b></font>
                     </td>
                 </tr>
 
                 <tr>
-                    <td align="left">Display Name</td>
-                    <td align="left">Type</td>
-                    <td align="left">References</td>
-                    <td>Multiple</td>
-                    <td>Mandatory</td>
-                    <td>Unique</td>
+                  <td cellspacing="0" colspan="6" align="center" bgcolor="${theme.table.row.bgcolor}">
+                    ${nodeLabel}
+                  </td>
                 </tr>
 
-                ${ createDotTableRows(node) }
+                <tr>
+                    <td align="left"><b>Display Name</b></td>
+                    <td align="left"><b>Type</b></td>
+                    <td align="left"><b>References</b></td>
+                    <td><b>Multiple</b></td>
+                    <td><b>Mandatory</b></td>
+                    <td><b>Unique</b></td>
+                </tr>
+
+                ${createDotTableRows(node)}
             </table>
         >];
     `
 }
 
 function createDotTableRows(node: DiagramNode) {
-    const fields = flattenFields(node.fields);
+  const fields = flattenFields(node.fields)
 
-    return fields.map((field: any, _: any) => {
-        return `<tr>
+  return fields.map((field: any, _: any) => {
+    return `<tr>
                     <td bgcolor="${theme.table.row.bgcolor}" align="left" port="${field.path}">${field.title}</td>
-                    <td bgcolor="${theme.table.row.bgcolor}" align="left">${field.type == null ? 'block' : field.type}</td>
-                    <td bgcolor="${theme.table.row.bgcolor}" align="left">${field.references.join(", ")}</td>
+                    <td bgcolor="${theme.table.row.bgcolor}" align="left">${field.type === null ? 'block' : field.type}</td>
+                    <td bgcolor="${theme.table.row.bgcolor}" align="left">${field.references.join(', ')}</td>
                     <td bgcolor="${theme.table.row.bgcolor}" align="center">${field.multiple ? theme.entities.hasOption : ''}</td>
                     <td bgcolor="${theme.table.row.bgcolor}" align="center">${field.mandatory ? theme.entities.hasOption : ''}</td>
                     <td bgcolor="${theme.table.row.bgcolor}" align="center">${field.unique ? theme.entities.hasOption : ''}</td>
                 </tr>`
-    }).join("\n")
+  }).join('\n')
 }
 
 function createDotEdges(nodes: DiagramNode[]) {
-    return nodes.map((node, _) => {
-        return createDotEdgeRows(node);
-    }).join("\n")
+  return nodes.map((node, _) => {
+    return createDotEdgeRows(node)
+  }).join('\n')
 }
 
 function createDotEdgeRows(node: DiagramNode) {
-    const result = [];
-    const fields = flattenFields(node.fields);
+  const result = []
+  const fields = flattenFields(node.fields)
 
-    for (const field of fields) {
-        if (field.type === 'reference') {
-            for (const reference of field.references) {
-                result.push(`${node.uid}:${field.path} -> ${reference}:${reference}`);
-            }
-        } else if (field.type === 'global_field') {
-            result.push(`${node.uid}:${field.path} -> ${field.references[0]}`);
-        }
+  for (const field of fields) {
+    if (field.type === 'reference') {
+      for (const reference of field.references) {
+        result.push(`${node.uid}:${field.path} -> ${reference}:${reference}`)
+      }
+    } else if (field.type === 'global_field') {
+      result.push(`${node.uid}:${field.path} -> ${field.references[0]}`)
     }
+  }
 
-    return result.join("\n");
+  return result.join('\n')
 }
 
 function createFieldDescriptor(field: DiagramNodeField, depth = 0, prefix = '') {
-    const spacer = theme.entities.spacer.repeat(depth === 0 ? 0 : depth * 2)
-    const title = `${prefix ? spacer : ''}${field.title}`
-    const path = `${prefix ? prefix + '_' : ''}${field.uid}`.replace(/\./g, "_")
+  const spacer = theme.entities.spacer.repeat(depth === 0 ? 0 : depth * 2)
+  const title = `${prefix ? spacer : ''}${field.title}`
+  const path = `${prefix ? prefix + '_' : ''}${field.uid}`.replace(/\./g, '_')
 
-    return {
-      uid: field.uid,
-      title: title,
-      type: field.type,
-      path: path,
-      references: field.references,
-      multiple: field.multiple,
-      mandatory: field.mandatory,
-      unique: field.unique
-    } as DiagramNodeField;
-  }
+  return {
+    uid: field.uid,
+    title: title,
+    type: field.type,
+    path: path,
+    references: field.references,
+    multiple: field.multiple,
+    mandatory: field.mandatory,
+    unique: field.unique,
+  } as DiagramNodeField
+}
 
 function flattenFields(fields: DiagramNodeField[], depth = 0, prefix = '') {
-    const rows = [] as DiagramNodeField[]
+  const rows = [] as DiagramNodeField[]
 
-    for (const field of fields) {
-        rows.push(createFieldDescriptor(field, depth, prefix))
+  for (const field of fields) {
+    rows.push(createFieldDescriptor(field, depth, prefix))
 
-        if (field.fields) {
-            rows.push(...flattenFields(field.fields, depth + 1, (prefix ? prefix + '.' : '') + field.uid))
-        }
+    if (field.fields) {
+      rows.push(...flattenFields(field.fields, depth + 1, (prefix ? prefix + '.' : '') + field.uid))
     }
+  }
 
-    return rows
+  return rows
+}
+
+function createOutputPath(outputFile: string) {
+  const outputPath = path.resolve(process.cwd(), outputFile)
+  const dirName = path.dirname(outputPath)
+
+  fs.mkdirSync(dirName, {recursive: true})
+
+  return outputPath
 }
