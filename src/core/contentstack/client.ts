4a3eed246aaffa4e78798f46bee03c3b5617baa8
naman-contentstack <naman.dembla@cotentstack.com>
2026-01-08 17:43:23 +0530

    add condition for headers in audit

--- src/core/contentstack/client.ts
+++ src/core/contentstack/client.ts
@@ -1,64 +1,80 @@
-import axios, {AxiosInstance} from 'axios'
-import ContentstackError from './error'
-import {cliux} from '@contentstack/cli-utilities'
+import axios, { AxiosInstance } from "axios";
+import ContentstackError from "./error";
+import { cliux } from "@contentstack/cli-utilities";
 
 export default class ContentstackClient {
-  private instance: AxiosInstance;
+  private readonly instance: AxiosInstance;
 
   constructor(cmaHost: string, authToken: string) {
+    const headerName = authToken.includes("Bearer")
+      ? "authorization"
+      : "authtoken";
     this.instance = axios.create({
       baseURL: `https://${cmaHost}/v3/`,
       headers: {
-        authtoken: authToken,
+        [headerName]: authToken,
       },
-    })
+    });
   }
 
-  async getContentTypeAuditLogs(api_key: string, uid: string, spinner: any): Promise<any> {
+  async getContentTypeAuditLogs(
+    api_key: string,
+    uid: string,
+    spinner: any
+  ): Promise<any> {
     try {
-      const response = await this.instance.get('/audit-logs',
-        {
-          params: {
-            query: {$and: [{module: 'content_type'}, {'metadata.uid': uid}]},
+      const response = await this.instance.get("/audit-logs", {
+        params: {
+          query: {
+            $and: [{ module: "content_type" }, { "metadata.uid": uid }],
           },
-          headers: {api_key},
-        })
+        },
+        headers: { api_key },
+      });
 
-      return response.data
+      return response.data;
     } catch (error) {
-      cliux.loaderV2('', spinner)
-      throw this.buildError(error, {api_key})
+      cliux.loaderV2("", spinner);
+      throw this.buildError(error, { api_key });
     }
   }
 
-  async getContentTypeReferences(api_key: string, uid: string, spinner: any): Promise<any> {
+  async getContentTypeReferences(
+    api_key: string,
+    uid: string,
+    spinner: any
+  ): Promise<any> {
     try {
-      const response = await this.instance.get(`/content_types/${uid}/references`, {
-        params: {
-          include_global_fields: true,
-        },
-        headers: {
-          api_key,
-        },
-      })
+      const response = await this.instance.get(
+        `/content_types/${uid}/references`,
+        {
+          params: {
+            include_global_fields: true,
+          },
+          headers: {
+            api_key,
+          },
+        }
+      );
 
-      return response.data
+      return response.data;
     } catch (error) {
-      cliux.loaderV2('', spinner)
-      throw this.buildError(error, {api_key})
+      cliux.loaderV2("", spinner);
+      throw this.buildError(error, { api_key });
     }
   }
 
   private buildError(error: any, context: any = {}) {
-    const data = error?.response?.data
-    if (!data || !data.errors) return new Error('Unrecognized error. Please try again.')
+    const data = error?.response?.data;
+    if (!data || !data.errors)
+      return new Error("Unrecognized error. Please try again.");
 
-    let message = data.error_message
-    const code = data.error_code
+    let message = data.error_message;
+    const code = data.error_code;
 
     if (data.errors.api_key && context.api_key) {
-      message += ` This is in regards to the Stack API Key '${context.api_key}'.`
+      message += ` This is in regards to the Stack API Key '${context.api_key}'.`;
     }
-    return new ContentstackError(message, code)
+    return new ContentstackError(message, code);
   }
 }
