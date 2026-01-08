cad16563cd604398b027db5d911f2bb3901f2edf
naman-contentstack <naman.dembla@cotentstack.com>
2026-01-08 17:00:18 +0530

    add authenticationHandler from utilities

--- src/commands/content-type/compare.ts
+++ src/commands/content-type/compare.ts
@@ -1,119 +1,134 @@
-import Command from '../../core/command'
-import { flags, FlagInput, managementSDKClient, cliux, printFlagDeprecation } from '@contentstack/cli-utilities'
-import buildOutput from '../../core/content-type/compare'
-import { getStack, getContentType } from '../../utils'
+import Command from "../../core/command";
+import {
+  flags,
+  FlagInput,
+  managementSDKClient,
+  cliux,
+  printFlagDeprecation,
+} from "@contentstack/cli-utilities";
+import buildOutput from "../../core/content-type/compare";
+import { getStack, getContentType } from "../../utils";
 
 export default class CompareCommand extends Command {
-  static description = 'Compare two Content Type versions'
+  static description = "Compare two Content Type versions";
 
   static examples = [
     '$ csdx content-type:compare --stack-api-key "xxxxxxxxxxxxxxxxxxx" --content-type "home_page"',
     '$ csdx content-type:compare --stack-api-key "xxxxxxxxxxxxxxxxxxx" --content-type "home_page" --left # --right #',
-    '$ csdx content-type:compare --alias "management token" --content-type "home_page" --left # --right #'
-  ]
+    '$ csdx content-type:compare --alias "management token" --content-type "home_page" --left # --right #',
+  ];
 
   static flags: any = {
     stack: flags.string({
-      char: 's',
-      description: 'Stack UID',
-      exclusive: ['token-alias'],
-      parse: printFlagDeprecation(['-s', '--stack'], ['-k', '--stack-api-key'])
+      char: "s",
+      description: "Stack UID",
+      exclusive: ["token-alias"],
+      parse: printFlagDeprecation(["-s", "--stack"], ["-k", "--stack-api-key"]),
     }),
 
-    'stack-api-key': flags.string({
-      char: 'k',
-      description: 'Stack API Key',
-      exclusive: ['token-alias']
+    "stack-api-key": flags.string({
+      char: "k",
+      description: "Stack API Key",
+      exclusive: ["token-alias"],
     }),
 
-    'token-alias': flags.string({
-      char: 'a',
-      description: 'Management token alias',
-      parse: printFlagDeprecation(['--token-alias'], ['-a', '--alias'])
+    "token-alias": flags.string({
+      char: "a",
+      description: "Management token alias",
+      parse: printFlagDeprecation(["--token-alias"], ["-a", "--alias"]),
     }),
 
     alias: flags.string({
-      char: 'a',
-      description: 'Alias of the management token'
+      char: "a",
+      description: "Alias of the management token",
     }),
 
-    'content-type': flags.string({
-      char: 'c',
-      description: 'Content Type UID',
+    "content-type": flags.string({
+      char: "c",
+      description: "Content Type UID",
       required: true,
-      parse: printFlagDeprecation(['-c'], ['--content-type'])
+      parse: printFlagDeprecation(["-c"], ["--content-type"]),
     }),
 
     left: flags.integer({
-      char: 'l',
-      description: 'Content Type version, i.e. prev version',
-      dependsOn: ['right'],
-      parse: printFlagDeprecation(['-l'], ['--left'])
+      char: "l",
+      description: "Content Type version, i.e. prev version",
+      dependsOn: ["right"],
+      parse: printFlagDeprecation(["-l"], ["--left"]),
     }),
 
     right: flags.integer({
-      char: 'r',
-      description: 'Content Type version, i.e. later version',
-      dependsOn: ['left'],
-      parse: printFlagDeprecation(['-r'], ['--right'])
-    })
-  }
+      char: "r",
+      description: "Content Type version, i.e. later version",
+      dependsOn: ["left"],
+      parse: printFlagDeprecation(["-r"], ["--right"]),
+    }),
+  };
 
   async run() {
     try {
-      const { flags } = await this.parse(CompareCommand)
-      this.setup(flags)
+      const { flags } = await this.parse(CompareCommand);
+      await this.setup(flags);
       this.contentTypeManagementClient = await managementSDKClient({
         host: this.cmaHost,
-        'X-CS-CLI': this.context?.analyticsInfo
-      })
+        "X-CS-CLI": this.context?.analyticsInfo,
+      });
 
       if (!flags.left) {
-        const spinner1 = cliux.loaderV2(Command.RequestDataMessage)
+        const spinner1 = cliux.loaderV2(Command.RequestDataMessage);
         const discovery = await getContentType({
           managementSdk: this.contentTypeManagementClient,
           apiKey: this.apiKey,
-          uid: flags['content-type'],
-          spinner: spinner1
-        })
-        const version = discovery._version
+          uid: flags["content-type"],
+          spinner: spinner1,
+        });
+        const version = discovery._version;
 
-        flags.left = version
-        flags.right = version > 1 ? version - 1 : version
-        cliux.loaderV2('', spinner1)
-        this.log(`Comparing versions: ${flags.left} <-> ${flags.right}.`)
+        flags.left = version;
+        flags.right = version > 1 ? version - 1 : version;
+        cliux.loaderV2("", spinner1);
+        this.log(`Comparing versions: ${flags.left} <-> ${flags.right}.`);
       }
 
-      const spinner = cliux.loaderV2(Command.RequestDataMessage)
+      const spinner = cliux.loaderV2(Command.RequestDataMessage);
 
       const [stack, previous, current] = await Promise.all([
         getStack(this.contentTypeManagementClient, this.apiKey, spinner),
         getContentType({
           managementSdk: this.contentTypeManagementClient,
           apiKey: this.apiKey,
-          uid: flags['content-type'],
+          uid: flags["content-type"],
           ctVersion: flags.left,
-          spinner
+          spinner,
         }),
         getContentType({
           managementSdk: this.contentTypeManagementClient,
           apiKey: this.apiKey,
-          uid: flags['content-type'],
+          uid: flags["content-type"],
           ctVersion: flags.right,
-          spinner
-        })
-      ])
+          spinner,
+        }),
+      ]);
 
-      cliux.loaderV2('', spinner)
+      cliux.loaderV2("", spinner);
 
-      const output = await buildOutput(flags['content-type'], previous, current)
-      this.printOutput(output, 'changes', flags['content-type'], stack.name)
+      const output = await buildOutput(
+        flags["content-type"],
+        previous,
+        current
+      );
+      this.printOutput(output, "changes", flags["content-type"], stack.name);
 
       if (flags.left && flags.left === flags.right) {
-        this.warn('You cannot compare the same version. Please select different versions to compare.')
+        this.warn(
+          "You cannot compare the same version. Please select different versions to compare."
+        );
       }
     } catch (error: any) {
-      this.error(error?.message || 'An error occurred.', { exit: 1, suggestions: error.suggestions })
+      this.error(error?.message || "An error occurred.", {
+        exit: 1,
+        suggestions: error.suggestions,
+      });
     }
   }
 }
--- src/commands/content-type/list.ts
+++ src/commands/content-type/list.ts
@@ -1,73 +1,82 @@
-import Command from '../../core/command'
-import { flags, FlagInput, managementSDKClient, cliux, printFlagDeprecation } from '@contentstack/cli-utilities'
-import buildOutput from '../../core/content-type/list'
-import { getStack, getContentTypes } from '../../utils'
+import Command from "../../core/command";
+import {
+  flags,
+  FlagInput,
+  managementSDKClient,
+  cliux,
+  printFlagDeprecation,
+} from "@contentstack/cli-utilities";
+import buildOutput from "../../core/content-type/list";
+import { getStack, getContentTypes } from "../../utils";
 
 export default class ListCommand extends Command {
-  static description = 'List all Content Types in a Stack'
+  static description = "List all Content Types in a Stack";
 
   static examples = [
     '$ csdx content-type:list --stack-api-key "xxxxxxxxxxxxxxxxxxx"',
     '$ csdx content-type:list --alias "management token"',
-    '$ csdx content-type:list --alias "management token" --order modified'
-  ]
+    '$ csdx content-type:list --alias "management token" --order modified',
+  ];
 
   static flags: any = {
     stack: flags.string({
-      char: 's',
-      description: 'Stack UID',
-      exclusive: ['token-alias', 'alias'],
-      parse: printFlagDeprecation(['-s', '--stack'], ['-k', '--stack-api-key'])
+      char: "s",
+      description: "Stack UID",
+      exclusive: ["token-alias", "alias"],
+      parse: printFlagDeprecation(["-s", "--stack"], ["-k", "--stack-api-key"]),
     }),
 
-    'stack-api-key': flags.string({
-      char: 'k',
-      description: 'Stack API Key',
-      exclusive: ['token-alias', 'alias']
+    "stack-api-key": flags.string({
+      char: "k",
+      description: "Stack API Key",
+      exclusive: ["token-alias", "alias"],
     }),
 
-    'token-alias': flags.string({
-      char: 'a',
-      description: 'Management token alias',
-      parse: printFlagDeprecation(['--token-alias'], ['-a', '--alias'])
+    "token-alias": flags.string({
+      char: "a",
+      description: "Management token alias",
+      parse: printFlagDeprecation(["--token-alias"], ["-a", "--alias"]),
     }),
 
     alias: flags.string({
-      char: 'a',
-      description: 'Alias of the management token'
+      char: "a",
+      description: "Alias of the management token",
     }),
 
     order: flags.string({
-      char: 'o',
-      description: 'order by column',
-      options: ['title', 'modified'],
-      default: 'title',
-      parse: printFlagDeprecation(['-o'], ['--order'])
-    })
-  }
+      char: "o",
+      description: "order by column",
+      options: ["title", "modified"],
+      default: "title",
+      parse: printFlagDeprecation(["-o"], ["--order"]),
+    }),
+  };
 
   async run() {
     try {
-      const { flags } = await this.parse(ListCommand)
-      this.setup(flags)
+      const { flags } = await this.parse(ListCommand);
+      await this.setup(flags);
       this.contentTypeManagementClient = await managementSDKClient({
         host: this.cmaHost,
-        'X-CS-CLI': this.context?.analyticsInfo
-      })
+        "X-CS-CLI": this.context?.analyticsInfo,
+      });
 
-      const spinner = cliux.loaderV2(Command.RequestDataMessage)
+      const spinner = cliux.loaderV2(Command.RequestDataMessage);
 
       const [stack, contentTypes] = await Promise.all([
         getStack(this.contentTypeManagementClient, this.apiKey, spinner),
-        getContentTypes(this.contentTypeManagementClient, this.apiKey, spinner)
-      ])
+        getContentTypes(this.contentTypeManagementClient, this.apiKey, spinner),
+      ]);
 
-      cliux.loaderV2('', spinner)
+      cliux.loaderV2("", spinner);
 
-      const output = buildOutput(contentTypes, flags.order)
-      this.printOutput(output, 'Content Types', null, stack.name)
+      const output = buildOutput(contentTypes, flags.order);
+      this.printOutput(output, "Content Types", null, stack.name);
     } catch (error: any) {
-      this.error(error?.message || 'An error occurred.', { exit: 1, suggestions: error.suggestions })
+      this.error(error?.message || "An error occurred.", {
+        exit: 1,
+        suggestions: error.suggestions,
+      });
     }
   }
 }
