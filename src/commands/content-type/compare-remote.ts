a9764acd4eed31ed44fed79f94cc75f243e630ac
harshitha-cstk <harshitha.d@contentstack.com>
2026-05-14 18:23:04 +0530

    refactor: update command flags for content-type commands to use long options and remove deprecated flags

--- src/commands/content-type/compare-remote.ts
+++ src/commands/content-type/compare-remote.ts
@@ -1,10 +1,8 @@
 import Command from "../../core/command";
 import {
   flags,
-  FlagInput,
   managementSDKClient,
   cliux,
-  printFlagDeprecation,
 } from "@contentstack/cli-utilities";
 import buildOutput from "../../core/content-type/compare";
 import { getStack, getContentType } from "../../utils";
@@ -13,38 +11,32 @@ export default class CompareRemoteCommand extends Command {
   static description = "compare two Content Types on different Stacks";
 
   static examples = [
-    '$ csdx content-type:compare-remote --origin-stack "xxxxxxxxxxxxxxxxxxx" --remote-stack "xxxxxxxxxxxxxxxxxxx" -content-type "home_page"',
+    '$ csdx content-type:compare-remote --origin-stack "xxxxxxxxxxxxxxxxxxx" --remote-stack "xxxxxxxxxxxxxxxxxxx" --content-type "home_page"',
   ];
 
   static flags: any = {
     "origin-stack": flags.string({
-      char: "o",
       description: "Origin Stack API Key",
       required: true,
       dependsOn: ["remote-stack"],
-      parse: printFlagDeprecation(["-o"], ["--remote-stack"]),
     }),
 
     "remote-stack": flags.string({
-      char: "r",
       description: "Remote Stack API Key",
       required: true,
       dependsOn: ["origin-stack"],
-      parse: printFlagDeprecation(["-r"], ["--remote-stack"]),
     }),
 
     "content-type": flags.string({
-      char: "c",
       description: "Content Type UID",
       required: true,
-      parse: printFlagDeprecation(["-c"], ["--content-type"]),
     }),
   };
 
   async run() {
     try {
       const { flags } = await this.parse(CompareRemoteCommand);
-      await this.setup({ alias: undefined, stack: flags["origin-stack"] });
+      await this.setup({ "stack-api-key": flags["origin-stack"] });
       this.contentTypeManagementClient = await managementSDKClient({
         host: this.cmaHost,
         "X-CS-CLI": this.context?.analyticsInfo,
